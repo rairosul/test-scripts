dbbf6b77588382902b9d3cd4e698d20532032fe4
Rúairí <rairosul@ie.ibm.com>
2017-12-14 15:38:52 +0000

    Fix syntax error

--- spec.js
+++ spec.js
@@ -3,7 +3,7 @@
 
 describe('Protector Demo App', function() {
 	it('should have a title', function() {
-		browser.get('http://www.example.com/'');
+		browser.get('http://www.example.com/');
 		expect(browser.getTitle()).toEqual('Example Domain');
 	});
 });
