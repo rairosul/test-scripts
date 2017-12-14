e34644d7c1c94a93f0446d150f27150762c38477
Rúairí <rairosul@ie.ibm.com>
2017-12-14 15:57:02 +0000

    Update script for angular website

--- spec.js
+++ spec.js
@@ -1,9 +1,9 @@
 // spec.js for protractor
 // This spec will check for titles of webpages
 
-describe('Protector Demo App', function() {
+describe('This webpage', function() {
 	it('should have a title', function() {
-		browser.get('http://www.ibm.com/uk-en/', 20000);
-		expect(browser.getTitle()).toEqual('IBM  - United Kingdom');
+		browser.get('https://docs.angularjs.org/api/', 20000);
+		expect(browser.getTitle()).toEqual('AngularJS: API: API Reference');
 	});
 });
