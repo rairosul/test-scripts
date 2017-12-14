54c9f5c2ccda9cd14b20a0a196fd760b09ecdab4
Rúairí <rairosul@ie.ibm.com>
2017-12-14 16:31:24 +0000

    Updated to fix test

---
+++ firsttest_spec.js
@@ -0,0 +1,9 @@
+// spec.js for protractor
+// This spec will check for titles of webpages
+
+describe('This webpage', function() {
+	it('should have a title', function() {
+		browser.get('https://docs.angularjs.org/api/', 20000);
+		expect(browser.getTitle()).toEqual('AngularJS: API: API Reference2');
+	});
+});
